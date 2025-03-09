eb5f0080fe87ff84e884c8b75c9ce96fe1c19fa3
AdyanshS <adyansh@sakarrobotics.com>
2025-03-09 20:03:02 +0530

    Implement ROS 2 integration with LED and button state management, including error handling and timer callbacks

--- vector_esp32io/include/configuration.h
+++ vector_esp32io/include/configuration.h
@@ -11,6 +11,30 @@ constexpr int relayPin3 = 3;
 // Button pins
 constexpr int buttonPin1 = 5;
 constexpr int buttonPin2 = 8;
+
+//. WIFI Configuration Parameters
+struct WifiConfig
+{
+  const char * ssid;     /**< Wifi network SSID */
+  const char * password; /**< Wifi network password */
+  uint8_t ip[4];         /**< Agent IP address represented as four octets */
+  size_t port;           /**< Agent port number */
+
+  inline IPAddress getAgentIP() const
+  {
+    return IPAddress(ip[0], ip[1], ip[2], ip[3]);
+  }
+};
+
+static constexpr WifiConfig WIFI_CONFIG = {
+  "adyansh_sakar",  // SSID
+  "12345678",       // Password
+  {10, 42, 0, 1},   // IP
+  8888              // Port
+};
+
+static constexpr int ROS_DOMAIN_ID = 20;
+
 }  // namespace Config
 
 #endif  // CONFIGURATION_H
--- vector_esp32io/src/main.cpp
+++ vector_esp32io/src/main.cpp
@@ -1,23 +1,264 @@
 #include <Arduino.h>
+#include <rclc/executor.h>
+#include <rclc/rclc.h>
 
 #include "button.h"
-const int buttonPin = 4;
+#include "configuration.h"
+#include "micro_ros_platformio.h"
+#include "relay.h"
+#include "vector_interfaces/msg/led_states.h"
+#include "vector_interfaces/msg/push_button_states.h"
 
-button p1(buttonPin);
+// . Macro Definations
+/**
+ * @brief Execute a function and abort on error
+ *
+ * Calls the expression and enters error loop if result isn't RCL_RET_OK
+ */
+#define RCCHECK(fn)                                                                                \
+  {                                                                                                \
+    rcl_ret_t temp_rc = fn;                                                                        \
+    if ((temp_rc != RCL_RET_OK)) {                                                                 \
+      rclErrorLoop();                                                                              \
+    }                                                                                              \
+  }
 
-void setup()
+/**
+ * @brief Execute a function with soft error checking
+ *
+ * Calls the expression without action if result isn't RCL_RET_OK
+ */
+#define RCSOFTCHECK(fn)                                                                            \
+  {                                                                                                \
+    rcl_ret_t temp_rc = fn;                                                                        \
+    if ((temp_rc != RCL_RET_OK)) {                                                                 \
+    }                                                                                              \
+  }
+
+/**
+ * @brief Executes code block every specified milliseconds
+ *
+ * Runs the given code X only if the elapsed time is greater than MS.
+ */
+#define EXECUTE_EVERY_N_MS(MS, X)                                                                  \
+  do {                                                                                             \
+    static volatile int64_t init = -1;                                                             \
+    if (init == -1) {                                                                              \
+      init = uxr_millis();                                                                         \
+    }                                                                                              \
+    if (uxr_millis() - init > MS) {                                                                \
+      X;                                                                                           \
+      init = uxr_millis();                                                                         \
+    }                                                                                              \
+  } while (0)
+
+//. Utility functions
+/**
+ * @brief  Flash the onboard LED a given number of times
+ *
+ * This function turns the LED on and off with a fixed delay
+ *
+ * @param n_times Number of times to flash the LED
+ */
+inline void flashLED(int n_times)
 {
-  Serial.begin(115200);
-  p1.setDebounceTime(50);
+  for (int i = 0; i < n_times; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(150);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(150);
+  }
+  delay(1000);
 }
 
-void loop()
+/**
+ * @brief Error loop function
+ *
+ * This function is called if an error is encountered during initialization.
+ * It continuously flashes the onboard LED.
+ */
+inline void rclErrorLoop()
 {
-  p1.loop();
-
-  if (p1.isPressed()) {
-    Serial.println("Button pressed");
-  } else if (p1.isReleased()) {
-    Serial.println("Button released");
+  while (true) {
+    flashLED(2);
   }
+}
+/**
+ * @brief State Machine States
+ *
+ * The state machine states for the main loop.
+ */
+enum states
+{
+  WAITING_AGENT,
+  AGENT_AVAILABLE,
+  AGENT_CONNECTED,
+  AGENT_DISCONNECTED
+};
+
+//* URos support structures
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rclc_executor_t executor_one;
+
+//* Message types
+vector_interfaces__msg__LedStates led_states_msg;
+vector_interfaces__msg__PushButtonStates push_button_states_msg;
+
+//* ROS 2 communication objects
+rcl_publisher_t push_button_states_publisher;
+rcl_subscription_t led_states_subscription;
+rcl_timer_t push_button_states_timer;
+
+// Relay - LEDs
+Relay redLed(Config::relayPin1);
+Relay greenLed(Config::relayPin2);
+Relay orangeLed(Config::relayPin3);
+
+// Push buttons
+button button1(Config::buttonPin1);
+button button2(Config::buttonPin2);
+
+// Previous LED states
+volatile bool prev_red_led_state    = false;
+volatile bool prev_green_led_state  = false;
+volatile bool prev_orange_led_state = false;
+
+void led_states_callback(const void * msgin)
+{
+  const vector_interfaces__msg__LedStates * led_states_msg =
+    (const vector_interfaces__msg__LedStates *)msgin;
+
+  //
+  bool red    = led_states_msg->red_led;
+  bool green  = led_states_msg->green_led;
+  bool orange = led_states_msg->orange_led;
+
+  // Process red LED state
+  if (prev_red_led_state != red) {
+    if (red) {
+      redLed.turnOn();
+    } else {
+      redLed.turnOff();
+    }
+  }
+
+  // Process green LED state
+  if (prev_green_led_state != green) {
+    if (green) {
+      greenLed.turnOn();
+    } else {
+      greenLed.turnOff();
+    }
+  }
+
+  // Process orange LED state
+  if (prev_orange_led_state != orange) {
+    if (orange) {
+      orangeLed.turnOn();
+    } else {
+      orangeLed.turnOff();
+    }
+  }
+
+  // (prev_red_led_state != red)    ? (red ? redLed.turnOn()    : redLed.turnOff())    : (void)0;
+  // (prev_green_led_state != green) ? (green ? greenLed.turnOn()  : greenLed.turnOff())  : (void)0;
+  // (prev_orange_led_state != orange)? (orange ? orangeLed.turnOn() : orangeLed.turnOff()) :
+  // (void)0;
+
+  // Update previous LED states
+  prev_red_led_state    = red;
+  prev_green_led_state  = green;
+  prev_orange_led_state = orange;
+}
+
+void push_button_states_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    // Populate the push button states message
+    push_button_states_msg.button1 = button1.getState();
+    push_button_states_msg.button2 = button2.getState();
+
+    // Publish the push button states message
+    RCSOFTCHECK(rcl_publish(&push_button_states_publisher, &push_button_states_msg, NULL));
+  }
+}
+
+void hardware_init()
+{
+  // Initialize the onboard LED
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Initialize the relay pins
+  redLed.begin();
+  greenLed.begin();
+  orangeLed.begin();
+
+  // Initialize the push button pins
+  button1.setDebounceTime(50);
+  button2.setDebounceTime(50);
+}
+
+bool create_entities()
+{
+  allocator = rcl_get_default_allocator();
+
+  // Create and modify init_options
+  rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+  RCCHECK(rcl_init_options_init(&init_options, allocator));
+  RCCHECK(rcl_init_options_set_domain_id(&init_options, Config::ROS_DOMAIN_ID));
+  RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
+
+  // Create Node
+  RCCHECK(rclc_node_init_default(&node, "vector_io_esp32", "", &support));
+
+  // Create Push Button States Publisher
+  RCCHECK(rclc_publisher_init_default(
+    &push_button_states_publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(vector_interfaces, msg, PushButtonStates),
+    "push_button_states"));  // Topic name
+
+  // Create LED States Subscription
+  RCCHECK(
+    rclc_subscription_init_default(&led_states_subscription,
+                                   &node,
+                                   ROSIDL_GET_MSG_TYPE_SUPPORT(vector_interfaces, msg, LedStates),
+                                   "led_states"));  // Topic name
+
+  // Create Push Button States Timer (at 20Hz -> 1000/50)
+  const unsigned int push_button_states_timer_timeout = 50;
+  RCCHECK(rclc_timer_init_default(&push_button_states_timer,
+                                  &support,
+                                  RCL_MS_TO_NS(push_button_states_timer_timeout),
+                                  push_button_states_timer_callback));
+
+  // Create Executor ONE with 2 handles (push button states timer + led states subscription)
+  executor_one = rclc_executor_get_zero_initialized_executor();
+  RCCHECK(rclc_executor_init(&executor_one, &support.context, 2, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor_one, &push_button_states_timer));
+  RCCHECK(rclc_executor_add_subscription(&executor_one,
+                                         &led_states_subscription,
+                                         &led_states_msg,
+                                         led_states_callback,
+                                         ON_NEW_DATA));
+
+  return true;
+}
+
+bool destroyEntities()
+{
+  rmw_context_t * rmw_context = rcl_context_get_rmw_context(&support.context);
+  (void)rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
+
+  rclc_executor_fini(&executor_one);
+  RCCHECK(rcl_publisher_fini(&push_button_states_publisher, &node));
+  RCCHECK(rcl_subscription_fini(&led_states_subscription, &node));
+  RCCHECK(rcl_timer_fini(&push_button_states_timer));
+  RCCHECK(rcl_node_fini(&node));
+  rclc_support_fini(&support);
+
+  return true;
 }
